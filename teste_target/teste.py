636fe8844d4099c6366b0db31a44da11ff8a7711
puruosso64 <puruoss@hotmail.com>
2024-10-01 11:01:30 -0300

    Questão 1 em andamento...

--- teste_target/teste.py
+++ teste_target/teste.py
@@ -1,3 +1,15 @@
+# QUESTÃO 1 #
+SOMA = 0; # numero subsequente a ser somado ao anterior
+SEQ = 0; # numero resultante da soma
+BASE = 0; # numero anterior
+
+NUMBER = input (f"Escolha uma numero e verificaremos se ele se encaixa na sequencia de Fibonacci: ");
+
+while SEQ >= 
+
+# QUESTÃO 2 #
+
+# QUESTÃO 3 #
 SOMA = 0;
 INDICE = 12;
 K = 1;
