a1095c0c572cd2f4697ee9e0f2ade483b5b6739f
puruosso64 <puruoss@hotmail.com>
2024-10-01 15:04:03 -0300

    Questão 1 e 2 prontas.

--- teste_target/teste.py
+++ teste_target/teste.py
@@ -1,40 +1,88 @@
-# QUESTÃO 1 #
+import os
+import time
+
+## UTILITIES ##
+# Clear Screen
+def cls():
+    os.system("cls");
+# Kbhit
+def kbhit():
+    input("\nPressione qualquer tecla para continuar...");
+# wait
+def wait(sec):
+    time.sleep(sec);
+# ---------- #
+
+
+## QUESTÃO 1 ##
 def questao1():
-    SOMA = 0; # numero subsequente a ser somado ao anterior
+    SOMA = 1; # numero subsequente a ser somado ao anterior
     SEQ = 0; # numero resultante da soma
     BASE = 0; # numero anterior
 
-    NUMBER = input (f"Escolha uma numero e verificaremos se ele se encaixa na sequencia de Fibonacci: ");
+    NUMBER = int(input(f"Escolha uma numero e verificaremos se ele se encaixa na sequencia de Fibonacci: "));
 
-    while SEQ >= 
+    print(f"Numero escolhido: ", NUMBER);
+    kbhit();
+    cls();
+    while SEQ < NUMBER:
+        SEQ = BASE + SOMA;
+        print(BASE, " + ", SOMA, " = ", SEQ);
+        wait(0.3);
+        BASE = SOMA;
+        SOMA = SEQ;
+    
+    if NUMBER == SEQ:
+        print(f"\nO numero escolhido, ", NUMBER, ", pertence a sequência de Fibonacci.");
+    else:
+        print(f"\nO numero escolhido, ", NUMBER, ", não pertence a sequência de Fibonacci.");
+    kbhit();
+    cls();
 
-# QUESTÃO 2 #
+
+## QUESTÃO 2 ##
 #def questao2():
 
-# QUESTÃO 3 #
+## QUESTÃO 3 ##
 def questao3():
     SOMA = 0;
     INDICE = 12;
     K = 1;
+    spc = 1;
 
     while K < INDICE:
         K = K + 1;
         SOMA = SOMA + K;
-        print(SOMA);
+        print(f" "*spc, SOMA);
+        spc += 1;
+        wait(0.3);
+    print("\nFinal do processamento: ", SOMA);
 
-# CODE FLOW #
-# Questão 1
-print("Questão 1 - Sequencia de Fibonacci")
+## CODE FLOW ##
+# Limpando terminal para
+#inicio limpo do programa
 cls();
 
 # Questão 1
-print("Questão 1 - Sequencia de Fibonacci")
+print("Questão 1 - Sequencia de Fibonacci");
+kbhit();
+cls();
+questao1();
+kbhit();
+
+# Questão 2
+print("Questão 2 - String 'A'");
+#questao2();
+#print("A letra 'a' apareceu ", RECORRE, " vezes.");
+kbhit();
 cls();
 
-# Questão 1
-print("Questão 1 - Sequencia de Fibonacci")
+# Questão 3
+print("Questão 3 - 'For/While'");
+questao3();
+kbhit();
 cls();
 
-# Quebra do aplicativo
-print("Código por Alvaro G. Ribeiro Netto", "\nPressione qualquer tecla para encerrar o programa...")
-input;
+## QUEBRA DO APLICATIVO ##
+print("Código por Alvaro G. Ribeiro Netto");
+input("\nPressione qualquer tecla para encerrar o programa...");
