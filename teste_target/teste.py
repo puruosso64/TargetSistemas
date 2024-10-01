9c8d3feec9a2f58a64696223ff8590a7ccca16a3
puruosso64 <149529457+puruosso64@users.noreply.github.com>
2024-10-01 13:50:47 -0300

    Qestão 1 + code flow

--- teste_target/teste.py
+++ teste_target/teste.py
@@ -1,20 +1,40 @@
 # QUESTÃO 1 #
-SOMA = 0; # numero subsequente a ser somado ao anterior
-SEQ = 0; # numero resultante da soma
-BASE = 0; # numero anterior
+def questao1():
+    SOMA = 0; # numero subsequente a ser somado ao anterior
+    SEQ = 0; # numero resultante da soma
+    BASE = 0; # numero anterior
 
-NUMBER = input (f"Escolha uma numero e verificaremos se ele se encaixa na sequencia de Fibonacci: ");
+    NUMBER = input (f"Escolha uma numero e verificaremos se ele se encaixa na sequencia de Fibonacci: ");
 
-while SEQ >= 
+    while SEQ >= 
 
 # QUESTÃO 2 #
+#def questao2():
 
 # QUESTÃO 3 #
-SOMA = 0;
-INDICE = 12;
-K = 1;
+def questao3():
+    SOMA = 0;
+    INDICE = 12;
+    K = 1;
 
-while K < INDICE:
-    K = K + 1;
-    SOMA = SOMA + K;
-    print(SOMA);
+    while K < INDICE:
+        K = K + 1;
+        SOMA = SOMA + K;
+        print(SOMA);
+
+# CODE FLOW #
+# Questão 1
+print("Questão 1 - Sequencia de Fibonacci")
+cls();
+
+# Questão 1
+print("Questão 1 - Sequencia de Fibonacci")
+cls();
+
+# Questão 1
+print("Questão 1 - Sequencia de Fibonacci")
+cls();
+
+# Quebra do aplicativo
+print("Código por Alvaro G. Ribeiro Netto", "\nPressione qualquer tecla para encerrar o programa...")
+input;
